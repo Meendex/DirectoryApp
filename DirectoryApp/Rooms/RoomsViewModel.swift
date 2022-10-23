4c5fc0d8a3bef4d9fe3d2f30b098fafa1a018350
Mindaugas B <mindaugas.balakauskas@gmail.com>
2022-10-23 16:11:41 +0100

    Added rooms viewModel

--- DirectoryApp/Rooms/RoomsViewModel.swift
+++ DirectoryApp/Rooms/RoomsViewModel.swift
@@ -6,8 +6,42 @@
 //
 
 import Foundation
+import Combine
 
+protocol RoomsViewModelIn {
+    func getRoomsAsync() async
+    //func getRoomsClosure()
+}
+protocol RoomsViewModelOut {
+    var roomsRecordCount: Int {get}
+    func getRoomsModel(index: Int) -> RoomResponse
+}
 
-class RoomsViewModel {
+final class RoomsViewModel {
     
+    @Published var rooms: [RoomResponse] = []
+    private let networkManager: NetworkManagerActions
+    
+    init(networkManager: NetworkManagerActions) {
+        self.networkManager = networkManager
+    }
+}
+extension RoomsViewModel: RoomsViewModelIn {
+    func getRoomsAsync() async {
+        do {
+            let data = try await
+            networkManager.getData(from: EndPoints.roomsURL)
+            let parseData = try
+            networkManager.parseJsonData(RoomResponse.self, data: data)
+        } catch {
+            print(error)
+        }
+    }
+}
+extension RoomsViewModel: RoomsViewModelOut {
+    var roomsRecordCount: Int { return rooms.count}
+    
+    func getRoomsModel(index: Int) -> RoomResponse {
+        return rooms[index]
+    }
 }
