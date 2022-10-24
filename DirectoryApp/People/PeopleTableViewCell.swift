8d3f2f36a55aa0999a53472e1c6c239e57b2fd1d
Mindaugas B <mindaugas.balakauskas@gmail.com>
2022-10-24 16:22:06 +0100

    Improved UI rooms and people now have title also improved peoples UI, needs to change date format

--- DirectoryApp/People/PeopleTableViewCell.swift
+++ DirectoryApp/People/PeopleTableViewCell.swift
@@ -12,11 +12,9 @@ class PeopleTableViewCell: UITableViewCell {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var createdAtLabel: UILabel!
     @IBOutlet weak var firstNameLabel: UILabel!
-    @IBOutlet weak var lastNameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var jobtitleLabel: UILabel!
     @IBOutlet weak var favoriteColorLabel: UILabel!
-    @IBOutlet weak var idLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -33,19 +31,19 @@ class PeopleTableViewCell: UITableViewCell {
         avatarImageView.image = nil
         createdAtLabel.text = "Created: "
         firstNameLabel.text = "First Name: "
-        lastNameLabel.text = "Last Name: "
+        
         emailLabel.text = "E-mail: "
         jobtitleLabel.text = "Job Title:"
-        favoriteColorLabel.text = "Favorite Color: "
-        idLabel.text = "ID: "
+        favoriteColorLabel.text = "Favorite Colour: "
+        
     }
     func updateDisplayData(with model: PeopleRespose) {
-        createdAtLabel.text = "Created: " + model.createdAt
-        firstNameLabel.text = "First Name: " + model.firstName
-        lastNameLabel.text = "Last Name: " + model.lastName
-        emailLabel.text = "E-mail: " + model.email
-        jobtitleLabel.text = "Job Title:" + model.jobtitle
-        favoriteColorLabel.text = "Favorite Color: " + model.favouriteColor
-        idLabel.text = "ID: " + model.id
+        createdAtLabel.text = "Joined at: " + model.createdAt
+        firstNameLabel.text = "Full name: " + model.firstName + " " + model.lastName
+        
+        emailLabel.text = model.email
+        jobtitleLabel.text = "Title: " + model.jobtitle
+        favoriteColorLabel.text = "Favorite colour: " + model.favouriteColor
+        
     }
 }
